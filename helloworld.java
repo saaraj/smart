8e07a38fd7e4695a5b4c11b8af372e538eebbf95
saaraj <31433433+saaraj@users.noreply.github.com>
2017-09-14 15:12:36 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main( String[] args ) {
+class HelloWorld {
+    static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.exit( 0 ); //success
     }
